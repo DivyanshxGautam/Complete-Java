8e9fc6b5fa36b554afb213840d41b79ddb68ae42
Divyansh <divyansh7192@gmail.com>
2025-09-14 17:27:18 +0530

    New seperate Arithmetic folder

---
+++ Arithmetic Operators/src/Augmented.java
@@ -0,0 +1,17 @@
+public class Augmented {
+    public static void main(String[] args) {
+        //Augmented Assignment Operators
+
+        int x = 10;
+        int y = 3;
+
+        //x += y;
+        //x -= y;
+        //x *= y;
+        //x = x/y;
+        x %= y;
+
+        System.out.println(x);
+
+    }
+}
---
+++ Arithmetic Operators/src/Increment.java
@@ -0,0 +1,12 @@
+public class Increment {
+    public static void main(String[] args) {
+        // Increment and Decrement Operators
+
+        int x = 1;
+
+        x++;
+        //x--;
+
+        System.out.println(x);
+    }
+}
