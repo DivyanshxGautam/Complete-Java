df18ecd7259598c5b0cf305837003f02f23a5dc7
Divyansh <divyansh7192@gmail.com>
2025-09-17 11:34:24 +0530

    Initial commit

---
+++ LOOPS/src/Break.java
@@ -0,0 +1,15 @@
+public class Break {
+    public static void main(String[] args) {
+
+        // break = break out of a loop (STOP)
+
+
+        for(int i = 0; i < 10; i++){
+
+            if(i==5){
+                break;
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
---
+++ LOOPS/src/Continue.java
@@ -0,0 +1,12 @@
+public class Continue {
+    public static void main(String[] args) {
+        // continue = skip current iteration of a loop (SKIP)
+
+        for(int i = 0;i<10; i++){
+            if(i==5){
+                continue;
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
