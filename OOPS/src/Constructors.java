b46daf3a6f3f366bfd8c66b62e1cee3c212defc1
Divyansh <divyansh7192@gmail.com>
2025-09-24 10:37:58 +0530

    Please see both Constructors.java and Student.java together as Student class contains the object and constructor that is used in Constructors.java

--- OOPS/src/Constructors.java
+++ OOPS/src/Constructors.java
@@ -1,2 +1,16 @@
 public class Constructors {
+    static void main() {
+        // constructor = A special method to initialize objects
+        //               You can pass arguments to a constructor
+        //               and set up initial values
+
+        Student student1 = new Student("Future",41,3.2);
+        Student student2 = new Student("Patrick",34,1.5);
+        Student student3 = new Student("Sandy",27,4.0);
+
+       student1.study();
+       student2.study();
+       student3.study();
+
+    }
 }
